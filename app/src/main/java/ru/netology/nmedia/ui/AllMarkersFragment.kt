adb17b16f1f630f38f5899a5c9203d8ff15e6599
nicys <nicys2476@yandex.ru>
2021-08-19 13:15:37 +0700

    added AllMarkersFragment.kt

---
+++ app/src/main/java/ru/netology/nmedia/ui/AllMarkersFragment.kt
@@ -0,0 +1,47 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.FragmentAllMarkersBinding
+import ru.netology.nmedia.ui.adapter.MarkerAdapter
+import ru.netology.nmedia.ui.adapter.OnInteractionListener
+import ru.netology.nmedia.ui.dto.Marker
+import ru.netology.nmedia.ui.viewmodel.MarkerViewModel
+
+class AllMarkersFragment : Fragment() {
+
+    private val viewModel: MarkerViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentAllMarkersBinding.inflate(inflater, container, false)
+
+        val adapter = MarkerAdapter(object : OnInteractionListener{
+
+            override fun onRemove(marker: Marker) {
+                viewModel.removedById(marker.id)
+            }
+
+            override fun onEdit(marker: Marker) {
+                viewModel.edit(marker)
+            }
+        })
+
+        binding.markersList.adapter = adapter
+        viewModel.data.observe(viewLifecycleOwner, { state ->
+            adapter.submitList(state.markers)
+        })
+
+        return binding.root
+    }
+}
