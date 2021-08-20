3dd87142f40ef9adbefe7d4ee418243862eeaf34
nicys <nicys2476@yandex.ru>
2021-08-20 20:04:06 +0700

    corrected

--- app/src/main/java/ru/netology/nmedia/ui/PlacesFragment.kt
+++ app/src/main/java/ru/netology/nmedia/ui/PlacesFragment.kt
@@ -16,39 +16,14 @@ class PlacesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentPlacesBinding.inflate(inflater, container, false)
         val viewModel by viewModels<MapViewModel>(ownerProducer = ::requireActivity)
         val adapter = PlacesAdapter { viewModel.selectPlace(it) }
 
         viewModel.places.observe(viewLifecycleOwner, adapter::submitList)
 
-        binding.markersList.adapter = adapter
-        viewModel.data.observe(viewLifecycleOwner, { state ->
-            adapter.submitList(state.markers)
-
-//        return FragmentPlacesBinding.inflate(inflater, container, false).root.also {
-//            it.adapter = adapter
-//            it.recycledViewPool.setMaxRecycledViews(R.layout.fragment_places, 5)
+        return FragmentPlacesBinding.inflate(inflater, container, false).root.also {
+            it.adapter = adapter
+            it.recycledViewPool.setMaxRecycledViews(R.layout.card_place, 5)
         }
-            return binding.root
     }
 }
-
-//val binding = FragmentAllMarkersBinding.inflate(inflater, container, false)
-//
-//val adapter = MarkerAdapter(object : OnInteractionListener{
-//
-//    override fun onRemove(marker: Marker) {
-//        viewModel.removedById(marker.id)
-//    }
-//
-//    override fun onEdit(marker: Marker) {
-//        viewModel.edit(marker)
-//    }
-//})
-//binding.markersList.adapter = adapter
-//viewModel.data.observe(viewLifecycleOwner, { state ->
-//    adapter.submitList(state.markers)
-//})
-//
-//return binding.root
