3fb6995b748a2b6feaf74db338a04e56638d41c4
nicys <nicys2476@yandex.ru>
2021-08-20 16:27:37 +0700

    corrected

--- app/src/main/java/ru/netology/nmedia/ui/viewmodel/MapViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/ui/viewmodel/MapViewModel.kt
@@ -6,16 +6,14 @@ import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.LatLng
 import ru.netology.nmedia.ui.dto.Place
 
+private val empty = Place(
+    name = "",
+    location = LatLng(0.0, 0.0)
+)
+
 class MapViewModel : ViewModel() {
-    private val _places = MutableLiveData(
-        List(1000) {
-            Place(
-                "Moscow $it", LatLng(
-                    54.9238448, 83.0808806
-                )
-            )
-        }
-    )
+    private val _places = MutableLiveData<List<Place>>()
+
     val places: LiveData<List<Place>>
         get() = _places
     private val _selectedPlace = MutableLiveData<Place>()
