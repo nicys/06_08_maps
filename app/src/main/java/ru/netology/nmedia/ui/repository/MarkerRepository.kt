694354c3e65015a45a052631ad0b071598f74cc3
nicys <nicys2476@yandex.ru>
2021-09-01 12:23:42 +0700

    corrected MarkerRepository.kt

--- app/src/main/java/ru/netology/nmedia/ui/repository/MarkerRepository.kt
+++ app/src/main/java/ru/netology/nmedia/ui/repository/MarkerRepository.kt
@@ -2,11 +2,11 @@ package ru.netology.nmedia.ui.repository
 
 import androidx.lifecycle.LiveData
 import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.ui.dto.Marker
 
 interface MarkerRepository {
-    fun getAll(): LiveData<List<Marker>>
-    fun addMarker(marker: Marker)
-    fun removeMarker(coord: LatLng)
-    fun snippetByCoord(coord: LatLng)
+    val data: Flow<List<Marker>>
+    fun save(marker: Marker)
+    fun removeById(id: Int)
 }
