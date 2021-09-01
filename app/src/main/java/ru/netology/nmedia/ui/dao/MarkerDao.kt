7be8ed8b8e85e21edde0ca8f5fde2bfb28696ac9
nicys <nicys2476@yandex.ru>
2021-09-01 11:59:38 +0700

    added MarkerDao.kt

--- app/src/main/java/ru/netology/nmedia/ui/dao/MarkerDao.kt
+++ app/src/main/java/ru/netology/nmedia/ui/dao/MarkerDao.kt
@@ -1,4 +1,21 @@
 package ru.netology.nmedia.ui.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.netology.nmedia.ui.entity.MarkerEntity
+
+@Dao
 interface MarkerDao {
+
+    @Query("SELECT * FROM MarkerEntity ORDER BY id DESC")
+    fun getAll(): Flow<List<MarkerEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(marker: MarkerEntity)
+
+    @Query("DELETE FROM MarkerEntity where id = :id")
+    suspend fun removedById(id: Int)
 }
