1f28bac4a825e2c6d4234c50c917d681a11b9786
nicys <nicys2476@yandex.ru>
2021-09-01 12:18:19 +0700

    added AppDb.kt

--- app/src/main/java/ru/netology/nmedia/ui/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/ui/db/AppDb.kt
@@ -1,4 +1,30 @@
 package ru.netology.nmedia.ui.db
 
-class AppDb {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.netology.nmedia.ui.dao.MarkerDao
+import ru.netology.nmedia.ui.entity.MarkerEntity
+
+@Database(entities = [MarkerEntity::class], version = 1, exportSchema = false)
+abstract class AppDb : RoomDatabase() {
+
+    abstract fun markerDao(): MarkerDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDb? = null
+
+        fun getInstance(context: Context): AppDb {
+            return instance ?: synchronized(this) {
+                instance ?: buildDataBase(context).also { instance = it}
+            }
+        }
+
+        private fun buildDataBase(context: Context) =
+            Room.databaseBuilder(context, AppDb::class.java, "app.db")
+                .build()
+    }
 }
